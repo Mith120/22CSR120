6af29ec3a37a12e1b4ce59bc0ee299d672936707
Mitharshana <mitharshana4@gmail.com>
2025-05-12 11:56:37 +0530

    added some time slots

--- stockprice/src/stockpage.js
+++ stockprice/src/stockpage.js
@@ -4,6 +4,17 @@ function Stock() {
       <div>
         <h1 className="heading1"> STOCKS </h1>
         <h3 className="slot">Time Slots </h3>
+        <div className="button">
+            <button >
+                12:00am - 1:00am
+            </button>
+            <button>
+                1:00am - 2:00am
+            </button>
+            <button onClick={'http://20.244.56.144/evaluation-service/stocks/:ticker?minutes=m'}>
+                2:00am - 3:00am
+            </button>
+        </div>
       </div>
     );
   }
